30ff51d9af04ce2de88c28bdfa77d14079933784
huzaifanur <huzaifa98nur@gmail.com>
2024-03-18 22:48:16 +0500

    fix window issue

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,14 +1,24 @@
-import Section1 from "@/components/sections/events/section1";
-import Section2 from "@/components/sections/events/section2";
-import Section3 from "@/components/sections/events/section3";
 import React from "react";
+import dynamic from 'next/dynamic'
+ 
+const Section1 = dynamic(() => import('@/components/sections/events/section1'), {
+  ssr: false,
+})
+const Section2 = dynamic(() => import('@/components/sections/events/section2'), {
+  ssr: false,
+})
+const Section3 = dynamic(() => import('@/components/sections/events/section3'), {
+  ssr: false,
+})
+
+
 
 const Events = () => {
   return (
     <>
-      {/* <Section1 />
+      <Section1 />
       <Section2 />
-      <Section3 /> */}
+      <Section3 />
     </>
   );
 };
